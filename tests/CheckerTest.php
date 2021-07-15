0f0e7764a2607fccccbd4e34e2584de13da283c0
Simon Crow <simon@mapletreedesign.com>
2021-07-15 21:43:16 +0100

    Added PHPunit & scaffolded Unit Tests

---
+++ tests/CheckerTest.php
@@ -0,0 +1,38 @@
+<?php declare(strict_types=1);
+
+namespace Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class CheckerTest extends TestCase
+{
+    public function testValidPalindrome(): void
+    {
+
+    }
+
+    public function testInvalidPalindrome(): void
+    {
+
+    }
+
+    public function testValidAnagram(): void
+    {
+
+    }
+
+    public function testInvalidAnagram(): void
+    {
+
+    }
+
+    public function testValidPangram(): void
+    {
+
+    }
+
+    public function testInvalidPangram(): void
+    {
+
+    }
+}
